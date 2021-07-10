J0anJosep/OpenTTD

Feature: Add a window for joining depots.

src/dock_gui.cpp
@@ -264,6 +264,7 @@ struct BuildDocksToolbarWindow : Window {
 		DeleteWindowById(WC_BUILD_STATION, TRANSPORT_WATER);
 		DeleteWindowById(WC_BUILD_DEPOT, TRANSPORT_WATER);
 		DeleteWindowById(WC_SELECT_STATION, 0);
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_SHIP);
 		DeleteWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -517,6 +518,10 @@ public:
 		UpdateDocksDirection();
 	}
 
+	virtual ~BuildDocksDepotWindow() {
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_SHIP);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		switch (widget) {
@@ -548,6 +553,7 @@ public:
 		switch (widget) {
 			case WID_BDD_X:
 			case WID_BDD_Y:
+				DeleteWindowById(WC_SELECT_DEPOT, VEH_SHIP);
 				this->RaiseWidget(_ship_depot_direction + WID_BDD_X);
 				_ship_depot_direction = (widget == WID_BDD_X ? AXIS_X : AXIS_Y);
 				this->LowerWidget(_ship_depot_direction + WID_BDD_X);

src/rail_gui.cpp
@@ -747,6 +747,7 @@ struct BuildRailToolbarWindow : Window {
 		DeleteWindowById(WC_BUILD_DEPOT, TRANSPORT_RAIL);
 		DeleteWindowById(WC_BUILD_WAYPOINT, TRANSPORT_RAIL);
 		DeleteWindowById(WC_SELECT_STATION, 0);
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
 		DeleteWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -1839,6 +1840,10 @@ struct BuildRailDepotWindow : public PickerWindowBase {
 		this->LowerWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);
 	}
 
+	virtual ~BuildRailDepotWindow() {
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		if (!IsInsideMM(widget, WID_BRAD_DEPOT_NE, WID_BRAD_DEPOT_NW + 1)) return;
@@ -1861,6 +1866,7 @@ struct BuildRailDepotWindow : public PickerWindowBase {
 			case WID_BRAD_DEPOT_SE:
 			case WID_BRAD_DEPOT_SW:
 			case WID_BRAD_DEPOT_NW:
+				DeleteWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
 				this->RaiseWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);
 				_build_depot_direction = (DiagDirection)(widget - WID_BRAD_DEPOT_NE);
 				this->LowerWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);

src/road_gui.cpp
@@ -595,6 +595,7 @@ struct BuildRoadToolbarWindow : Window {
 		DeleteWindowById(WC_TRUCK_STATION, TRANSPORT_ROAD);
 		DeleteWindowById(WC_BUILD_DEPOT, TRANSPORT_ROAD);
 		DeleteWindowById(WC_SELECT_STATION, 0);
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_ROAD);
 		DeleteWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -994,6 +995,10 @@ struct BuildRoadDepotWindow : public PickerWindowBase {
 		this->FinishInitNested(TRANSPORT_ROAD);
 	}
 
+	virtual ~BuildRoadDepotWindow() {
+		DeleteWindowById(WC_SELECT_DEPOT, VEH_ROAD);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		if (!IsInsideMM(widget, WID_BROD_DEPOT_NE, WID_BROD_DEPOT_NW + 1)) return;
@@ -1016,6 +1021,7 @@ struct BuildRoadDepotWindow : public PickerWindowBase {
 			case WID_BROD_DEPOT_NE:
 			case WID_BROD_DEPOT_SW:
 			case WID_BROD_DEPOT_SE:
+				DeleteWindowById(WC_SELECT_DEPOT, VEH_ROAD);
 				this->RaiseWidget(_road_depot_orientation + WID_BROD_DEPOT_NE);
 				_road_depot_orientation = (DiagDirection)(widget - WID_BROD_DEPOT_NE);
 				this->LowerWidget(_road_depot_orientation + WID_BROD_DEPOT_NE);

OpenTTD/OpenTTD-Temp

src/dock_gui.cpp
@@ -265,6 +265,7 @@ struct BuildDocksToolbarWindow : Window {
 		CloseWindowById(WC_BUILD_STATION, TRANSPORT_WATER);
 		CloseWindowById(WC_BUILD_DEPOT, TRANSPORT_WATER);
 		CloseWindowById(WC_SELECT_STATION, 0);
+		CloseWindowById(WC_SELECT_DEPOT, VEH_SHIP);
 		CloseWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -519,6 +520,11 @@ public:
 		UpdateDocksDirection();
 	}
 
+	virtual ~BuildDocksDepotWindow()
+	{
+		CloseWindowById(WC_SELECT_DEPOT, VEH_SHIP);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		switch (widget) {
@@ -550,6 +556,7 @@ public:
 		switch (widget) {
 			case WID_BDD_X:
 			case WID_BDD_Y:
+				CloseWindowById(WC_SELECT_DEPOT, VEH_SHIP);
 				this->RaiseWidget(_ship_depot_direction + WID_BDD_X);
 				_ship_depot_direction = (widget == WID_BDD_X ? AXIS_X : AXIS_Y);
 				this->LowerWidget(_ship_depot_direction + WID_BDD_X);

src/rail_gui.cpp
@@ -748,6 +748,7 @@ struct BuildRailToolbarWindow : Window {
 		CloseWindowById(WC_BUILD_DEPOT, TRANSPORT_RAIL);
 		CloseWindowById(WC_BUILD_WAYPOINT, TRANSPORT_RAIL);
 		CloseWindowById(WC_SELECT_STATION, 0);
+		CloseWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
 		CloseWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -1884,6 +1885,10 @@ struct BuildRailDepotWindow : public PickerWindowBase {
 		this->LowerWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);
 	}
 
+	virtual ~BuildRailDepotWindow() {
+		CloseWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		if (!IsInsideMM(widget, WID_BRAD_DEPOT_NE, WID_BRAD_DEPOT_NW + 1)) return;
@@ -1906,6 +1911,7 @@ struct BuildRailDepotWindow : public PickerWindowBase {
 			case WID_BRAD_DEPOT_SE:
 			case WID_BRAD_DEPOT_SW:
 			case WID_BRAD_DEPOT_NW:
+				CloseWindowById(WC_SELECT_DEPOT, VEH_TRAIN);
 				this->RaiseWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);
 				_build_depot_direction = (DiagDirection)(widget - WID_BRAD_DEPOT_NE);
 				this->LowerWidget(_build_depot_direction + WID_BRAD_DEPOT_NE);

src/road_gui.cpp
@@ -596,6 +596,7 @@ struct BuildRoadToolbarWindow : Window {
 		CloseWindowById(WC_TRUCK_STATION, TRANSPORT_ROAD);
 		CloseWindowById(WC_BUILD_DEPOT, TRANSPORT_ROAD);
 		CloseWindowById(WC_SELECT_STATION, 0);
+		CloseWindowById(WC_SELECT_DEPOT, VEH_ROAD);
 		CloseWindowByClass(WC_BUILD_BRIDGE);
 	}
 
@@ -995,6 +996,10 @@ struct BuildRoadDepotWindow : public PickerWindowBase {
 		this->FinishInitNested(TRANSPORT_ROAD);
 	}
 
+	virtual ~BuildRoadDepotWindow() {
+		CloseWindowById(WC_SELECT_DEPOT, VEH_ROAD);
+	}
+
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
 	{
 		if (!IsInsideMM(widget, WID_BROD_DEPOT_NE, WID_BROD_DEPOT_NW + 1)) return;
@@ -1017,6 +1022,7 @@ struct BuildRoadDepotWindow : public PickerWindowBase {
 			case WID_BROD_DEPOT_NE:
 			case WID_BROD_DEPOT_SW:
 			case WID_BROD_DEPOT_SE:
+				CloseWindowById(WC_SELECT_DEPOT, VEH_ROAD);
 				this->RaiseWidget(_road_depot_orientation + WID_BROD_DEPOT_NE);
 				_road_depot_orientation = (DiagDirection)(widget - WID_BROD_DEPOT_NE);
 				this->LowerWidget(_road_depot_orientation + WID_BROD_DEPOT_NE);