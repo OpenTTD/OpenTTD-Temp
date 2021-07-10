J0anJosep/OpenTTD

Add: Set reservation and penalties to improve pathfinding in extended depots.

src/train_placement.cpp
@@ -177,6 +177,7 @@ void TrainPlacement::LiftTrain(Train *train, DoCommandFlag flags)
 	if ((flags & DC_EXEC) == 0) return;
 
 	SetPlatformReservation(train->tile, false);
+	SetBigDepotReservation(train, false);
 
 	UpdateSignalsOnSegment(train->tile, INVALID_DIAGDIR, train->owner);
 }
@@ -299,6 +300,7 @@ void TrainPlacement::PlaceTrain(Train *train, DoCommandFlag flags)
 	}
 
 	SetPlatformReservation(train->tile, true);
+	SetBigDepotReservation(train, true);
 
 	UpdateSignalsOnSegment(train->tile, INVALID_DIAGDIR, train->owner);
 }

OpenTTD/OpenTTD-Temp

src/train_placement.cpp
@@ -185,6 +185,7 @@ void TrainPlacement::LiftTrain(Train *train, DoCommandFlag flags)
 	if ((flags & DC_EXEC) == 0) return;
 
 	SetPlatformReservation(train->tile, false);
+	UpdateExtendedDepotReservation(train, false);
 
 	UpdateSignalsOnSegment(train->tile, INVALID_DIAGDIR, train->owner);
 }
@@ -307,6 +308,7 @@ void TrainPlacement::PlaceTrain(Train *train, DoCommandFlag flags)
 	}
 
 	SetPlatformReservation(train->tile, true);
+	UpdateExtendedDepotReservation(train, true);
 
 	UpdateSignalsOnSegment(train->tile, INVALID_DIAGDIR, train->owner);
 }