J0anJosep/OpenTTD

Add: Set reservation of big ship depots and prepare for setting it for road and train depots.

src/depot.cpp
@@ -152,3 +152,33 @@ void Depot::RescanDepotTiles()
 		InvalidateWindowData(WC_BUILD_VEHICLE, this->index, 0, true);
 	}
 }
+
+/**
+ * Fix tile reservations on big depots and vehicle changes.
+ * @param v Vehicle to be revised.
+ * @param reserve Whether to reserve or free the position v is occupying.
+ */
+void SetBigDepotReservation(Vehicle *v, bool reserve)
+{
+	assert(v != nullptr);
+	assert(IsBigDepotTile(v->tile));
+	DepotReservation res_type = DEPOT_RESERVATION_EMPTY;
+
+	res_type = (v->vehstatus & VS_STOPPED) ?
+			DEPOT_RESERVATION_FULL_STOPPED_VEH : DEPOT_RESERVATION_IN_USE;
+
+	switch (v->type) {
+		case VEH_ROAD:
+			break;
+
+		case VEH_SHIP:
+			SetDepotReservation(v->tile, res_type);
+			break;
+
+		case VEH_TRAIN:
+			break;
+
+
+		default: NOT_REACHED();
+	}
+}

src/depot_map.h
@@ -162,4 +162,6 @@ static inline void SetDepotReservation(TileIndex t, DepotReservation reservation
 	SB(_m[t].m4, 6, 2, reservation);
 }
 
+void SetBigDepotReservation(Vehicle *v, bool state);
+
 #endif /* DEPOT_MAP_H */

OpenTTD/OpenTTD-Temp

src/depot.cpp
@@ -159,3 +159,33 @@ void Depot::RescanDepotTiles()
 		InvalidateWindowData(WC_BUILD_VEHICLE, this->index, 0, true);
 	}
 }
+
+/**
+ * Fix tile reservations on big depots and vehicle changes.
+ * @param v Vehicle to be revised.
+ * @param reserve Whether to reserve or free the position v is occupying.
+ */
+void UpdateExtendedDepotReservation(Vehicle *v, bool reserve)
+{
+	assert(v != nullptr);
+	assert(IsBigDepotTile(v->tile));
+	DepotReservation res_type = DEPOT_RESERVATION_EMPTY;
+
+	res_type = (v->vehstatus & VS_STOPPED) ?
+			DEPOT_RESERVATION_FULL_STOPPED_VEH : DEPOT_RESERVATION_IN_USE;
+
+	switch (v->type) {
+		case VEH_ROAD:
+			break;
+
+		case VEH_SHIP:
+			SetDepotReservation(v->tile, res_type);
+			break;
+
+		case VEH_TRAIN:
+			break;
+
+
+		default: NOT_REACHED();
+	}
+}

src/depot_map.h
@@ -174,4 +174,6 @@ static inline void SetDepotReservation(TileIndex t, DepotReservation reservation
 	SB(_m[t].m4, 6, 2, reservation);
 }
 
+void UpdateExtendedDepotReservation(Vehicle *v, bool state);
+
 #endif /* DEPOT_MAP_H */